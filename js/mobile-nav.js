7d68afe74b54f668dbbc8d62b50c4514bd6a915a
Anatoly <anatolykostatrev@gmail.com>
2021-10-28 14:07:32 +0300

    Add func for aside Nav block

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,22 +1,73 @@
 "use strict";
 
 const btnBar = document.querySelector(".js-bar");
+const btnFooter = document.querySelector(".js-footer_bar");
 const asideNav = document.querySelector(".js-main_mobile");
-const btnMobile = document.querySelector(".js-mobile_bar");
+const asideFooterNav = document.querySelector(".js-footer_mobile");
 
-function openAsideNav() {
+const btnMobile = document.querySelector(".js-mobile_bar");
+const btnMobileFooter = document.querySelector(".js-mobile_footer_bar");
+
+const openTopNav = (e) => {
   btnBar.style.opacity = 0;
   btnBar.style.transition = "0.5s linear";
 
-  asideNav.style.transform = "translateY(0)";
+  asideNav.style.transform = "translateX(0)";
   asideNav.style.opacity = 1;
-}
 
-function closeAsideNav() {
+  if (window.innerWidth <= 992) {
+    document.body.style.overflow = "hidden";
+  }
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+  } else {
+    // старый IE
+    document.selection.empty();
+  }
+};
+
+const openFooterNav = (e) => {
+  btnFooter.style.opacity = 0;
+  btnFooter.style.transition = "0.5s linear";
+
+  asideFooterNav.style.transform = "translateX(0)";
+  asideFooterNav.style.opacity = 1;
+
+  if (window.innerWidth <= 992) {
+    document.body.style.overflow = "hidden";
+  }
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+  } else {
+    // старый IE
+    document.selection.empty();
+  }
+};
+
+const closeTopNav = (e) => {
   btnBar.style.opacity = 1;
-  asideNav.style.transform = "translateY(100%)";
+  asideNav.style.transform = "translateX(100%)";
   asideNav.style.opacity = 0;
-}
+  document.body.style.overflow = "";
+};
 
-btnBar.addEventListener("click", openAsideNav);
-btnMobile.addEventListener("click", closeAsideNav);
+const closeFooterNav = (e) => {
+  btnFooter.style.opacity = 1;
+  asideFooterNav.style.transform = "translateX(100%)";
+  asideFooterNav.style.opacity = 0;
+  document.body.style.overflow = "";
+};
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  prevScrollpos < currentScrollPos
+    ? (asideNav.style.transform = "translateY(-100%)")
+    : (asideNav.style.transform = "translateY(0)");
+};
+
+btnBar.addEventListener("click", openTopNav);
+btnFooter.addEventListener("click", openFooterNav);
+
+btnMobile.addEventListener("click", closeTopNav);
+btnMobileFooter.addEventListener("click", closeFooterNav);
