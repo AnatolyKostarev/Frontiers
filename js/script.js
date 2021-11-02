6a6b1e5acf677247622e9f7803c7756cce772be9
Anatoly <anatolykostatrev@gmail.com>
2021-11-02 18:53:45 +0300

    Add script for screens 769-992px for investor block

--- js/script.js
+++ js/script.js
@@ -5,18 +5,26 @@ const entrepreneurLink = document.querySelector(".js-entrepreneur");
 const entrepreneurList = document.querySelector(".js-entrepreneurServiceList");
 const investorList = document.querySelector(".js-investorServiceList");
 
-// entrepreneurList.style.display = "none";
-
 const showInvestor = (e) => {
   e.preventDefault();
   investorList.style.transform = "translateX(0)";
   entrepreneurList.style.transform = "translateX(100%)";
+
+  if (window.innerWidth >= 769 && window.innerWidth <= 1439) {
+    investorList.style.transform = "translateX(0)";
+    entrepreneurList.style.transform = "translate3d(130%, -137%, 0)";
+  }
 };
 
 const shownEtrepreneur = (e) => {
   e.preventDefault();
   investorList.style.transform = "translateX(-200%)";
   entrepreneurList.style.transform = "translateX(-100%)";
+
+  if (window.innerWidth >= 769 && window.innerWidth <= 1439) {
+    investorList.style.transform = "translateX(-200%)";
+    entrepreneurList.style.transform = "translate3d(0, -137%, 0)";
+  }
 };
 
 investorLink.addEventListener("click", showInvestor);
